518c0f3fe90296bfb3614116bea53b6185e2d342
space-walker11 <kaylidollard@gmail.com>
2024-03-23 13:51:24 -0600

    Update script.js

--- Week 8/script.js
+++ Week 8/script.js
@@ -6,11 +6,11 @@ const errorElement = document.getElementById('error')
 form.addEventListener('submit', (e) => {
   let messages = []
   if (name.value === '' || name.value == null) {
-    messages.push('Must enter your name.')
+      info.style.backgroundColor = "red"
   }
 
   if (phone.value === '' || phone.value == null) {
-    messages.push('Must enter you phone number.')
+    info.style.backgroundColor = "red"
   }
 
   if (messages.length > 0) {
