3108adb6ab5ab87f0d105eea659242219d35088a
space-walker11 <kaylidollard@gmail.com>
2024-04-26 14:26:35 -0600

    Update sketch.js

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -5,14 +5,14 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1280, 1280);
+  createCanvas(500, 500);
   // Define clickable areas here
   // Example: { x1: 100, y1: 150, x2: 200, y2: 250, info: 'Room 1' }
 }
 
 function draw() {
   background(255);
-  image(GeneMap, 0, 0, 1280, 1280); // Adjust if you need to scale the image
+  image(GeneMap, 0, 0, width, height); // Adjust if you need to scale the image
 
   // Optional: Highlight area on hover
   // checkIfMouseIsOverClickableArea();
