236fc41a2ff71daf0c6f94f80e70f222a0c453b9
space-walker11 <kaylidollard@gmail.com>
2024-04-26 13:59:55 -0600

    image

---
+++ Final Project/sketch.js
@@ -0,0 +1,19 @@
+let GeneMap;
+
+function preload() {
+  GeneMap = loadImage('mapforGE.png'); // Make sure to use the correct path to your image file
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // Define clickable areas here
+  // Example: { x1: 100, y1: 150, x2: 200, y2: 250, info: 'Room 1' }
+}
+
+function draw() {
+  background(255);
+  image(GeneMap, 0, 0, width, height); // Adjust if you need to scale the image
+
+  // Optional: Highlight area on hover
+  // checkIfMouseIsOverClickableArea();
+}
