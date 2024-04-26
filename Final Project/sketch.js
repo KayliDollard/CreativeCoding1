77fbbef3a53b9a265e26316877a446f317b9246e
space-walker11 <kaylidollard@gmail.com>
2024-04-26 14:42:40 -0600

    Update sketch.js

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -12,9 +12,21 @@ function setup() {
 
 function draw() {
   background(255);
+  
+  // Calculate the scale factor to fit the image within the canvas
+  let scaleFactor = min(width / GeneMap.width, height / GeneMap.height);
+  
+  // Calculate the scaled dimensions
+  let scaledWidth = GeneMap.width * scaleFactor;
+  let scaledHeight = GeneMap.height * scaleFactor;
+  
+  // Calculate the position to center the image
+  let posX = width / 2;
+  let posY = height / 2;
+  
+  // Draw the image
   imageMode(CENTER);
-  image(GeneMap, 500, 500);
-
+  image(GeneMap, posX, posY, scaledWidth, scaledHeight);
 
   // Optional: Highlight area on hover
   // checkIfMouseIsOverClickableArea();
