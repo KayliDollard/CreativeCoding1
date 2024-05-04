d5e1602283c0c2d9edf5d9ec0d498b1da2a94899
space-walker11 <kaylidollard@gmail.com>
2024-05-04 15:21:49 -0600

    Update sketch.js

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -28,19 +28,25 @@ function draw() {
   imageMode(CENTER);
   image(GeneMap, posX, posY, scaledWidth, scaledHeight);
 
-  //perimiter
-  noFill();
-  push()
+  // Circle 1 (Outer Circle)
+  if (dist(mouseX, mouseY, 764, 359) < 680 / 2) {
+    fill(255, 204, 0); // Change fill color to yellow when mouse is over
+  } else {
+    noFill(); // No fill if mouse is not over
+  }
   strokeWeight(4);
   stroke(192, 57, 43);
   circle(764, 359, 680);
-
-  //garbage
-  noFill();
-  push()
-  strokeWeight(4);
-  stroke(192, 57, 43);
-  circle(765, 359, 550);
+  
+  // Circle 2 (Inner Circle)
+  if (dist(mouseX, mouseY, 765, 359) >= 677 / 2 && dist(mouseX, mouseY, 765, 359) < 500 / 2) {
+    fill(0, 102, 255); // Fill with blue when mouse is over inner circle
+    ellipseMode(CENTER);
+    noStroke();
+    erase(); // Erase background in the area of the inner circle
+    ellipse(765, 359, 550, 550);
+    noErase(); // Stop erasing
+  }
 
   //the den
 
