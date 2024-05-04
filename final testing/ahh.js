30f12d92e82c74b4d67cb93317cebb24cda0963a
space-walker11 <kaylidollard@gmail.com>
2024-05-04 17:26:50 -0600

    testung

--- final testing/ahh.js
+++ final testing/ahh.js
@@ -1,4 +1,8 @@
+let GeneMap;
 let pieSlices = [];
+
+function preload() {
+  GeneMap = loadImage('mapforGE.png');}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -6,19 +10,30 @@ function setup() {
   // Creating pie slices
   pieSlices.push(new PieSlice(765, 359, 550, radians(0), radians(72), color(255, 0, 0), "Dr. Reds"));
   pieSlices.push(new PieSlice(765, 359, 550, radians(72), radians(144), color(0, 255, 0), "The Den"));
-  pieSlices.push(new PieSlice(765, 359, 550, radians(144), radians(216), color(0, 0, 255), "The Pit"));
-  pieSlices.push(new PieSlice(765, 359, 550, radians(216), radians(288), color(255, 255, 0), "Old Town"));
-  pieSlices.push(new PieSlice(765, 359, 550, radians(288), radians(360), color(255, 0, 255), "Suburbs"));
+  pieSlices.push(new PieSlice(765, 359, 550, radians(144), radians(216), color(0, 0, 255), "Old Town"));
+  pieSlices.push(new PieSlice(765, 359, 550, radians(216), radians(288), color(255, 255, 0), "Suburbs"));
+  pieSlices.push(new PieSlice(765, 359, 550, radians(288), radians(360), color(255, 0, 255), "The Pit"));
 }
 
 function draw() {
   background(0);
   textSize(20);
 
-  strokeWeight(1)
-  stroke(192, 57, 43)
+  strokeWeight(1);
+  stroke(192, 57, 43);
   text("X: " + mouseX, 100, 200);
   text("Y: " + mouseY, 100, 220);
+
+  let scaleFactor = min(width / GeneMap.width, height / GeneMap.height);
+  
+  let scaledWidth = GeneMap.width * scaleFactor;
+  let scaledHeight = GeneMap.height * scaleFactor;
+  
+  let posX = width / 2;
+  let posY = height / 2;
+  
+  imageMode(CENTER);
+  image(GeneMap, posX, posY, scaledWidth, scaledHeight);
 
   // Display and check mouse over for each pie slice
   for (let slice of pieSlices) {
@@ -35,29 +50,33 @@ class PieSlice {
     this.diameter = diameter;
     this.startAngle = startAngle;
     this.endAngle = endAngle;
-    this.fillColor = fillColor;
+    this.fillColor = color(0); // Set initial color to black
+    this.originalColor = fillColor; // Store the original color
     this.name = name;
+    this.displayText = false; // Initially, the name text is not displayed
   }
 
   // Display method
   display() {
     fill(this.fillColor);
     arc(this.x, this.y, this.diameter, this.diameter, this.startAngle, this.endAngle, PIE);
+    if (this.displayText) {
+      fill(255);
+      textAlign(CENTER, CENTER);
+      text(this.name, this.x + this.diameter / 4 * cos((this.startAngle + this.endAngle) / 2), this.y + this.diameter / 4 * sin((this.startAngle + this.endAngle) / 2));
+    }
   }
 
   // Check mouse over method
   checkMouseOver() {
     if (dist(mouseX, mouseY, this.x, this.y) < this.diameter / 2) {
-      // If mouse is over, display slice name
-      fill(255);
-      textSize(20);
-      textAlign(CENTER, CENTER);
-      // Calculate midpoint angle
-      let midAngle = (this.startAngle + this.endAngle) / 2;
-      // Calculate midpoint coordinates
-      let midX = this.x + (this.diameter / 2) * cos(midAngle);
-      let midY = this.y + (this.diameter / 2) * sin(midAngle);
-      text(this.name, midX, midY);
+      // If mouse is over, display name text
+      this.fillColor = this.originalColor; // Change color to original when mouse is over
+      this.displayText = true;
+    } else {
+      // If mouse is not over, revert to black color and hide name text
+      this.fillColor = color(0);
+      this.displayText = false;
     }
   }
 }
