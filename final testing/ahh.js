665c8c908a8965b391d0c0cacacc72b3a6e41ba3
space-walker11 <kaylidollard@gmail.com>
2024-05-05 23:43:39 -0600

    Update ahh.js

--- final testing/ahh.js
+++ final testing/ahh.js
@@ -1,6 +1,9 @@
 let GeneMap;
 let pieSlices = [];
-let innerCircleRadius = 550; // Radius of the inner circle
+let innerCircleRadius = 550;
+let garbageSectorText;
+let titleText;
+let subTitleText;
 
 function preload() {
   GeneMap = loadImage('mapforGE.png');
@@ -15,20 +18,20 @@ function setup() {
   pieSlices.push(new PieSlice(765, 359, innerCircleRadius, radians(216), radians(288), color('navy'), "Suburbs"));
   pieSlices.push(new PieSlice(765, 359, innerCircleRadius, radians(288), radians(360), color('lightcoral'), "The Pit")); 
 
-  // Draw line around the outside of the inner circle
-  let numOfPoints = 360; // Number of points to draw the line
-  let angleIncrement = TWO_PI / numOfPoints;
-  let centerX = 765;
-  let centerY = 359;
+  garbageSectorText = createP('Garbage Sector');
+  garbageSectorText.position(705, 19); // Move up by 20 and to the left by 20 again
+  garbageSectorText.style('color', 'black');
+  garbageSectorText.style('font-size', '30px');
 
-  stroke(255); // Set line color to white
-  for (let i = 0; i < numOfPoints; i++) {
-    let x1 = centerX + innerCircleRadius * cos(angleIncrement * i);
-    let y1 = centerY + innerCircleRadius * sin(angleIncrement * i);
-    let x2 = centerX + (innerCircleRadius + 20) * cos(angleIncrement * i); // 20 is the distance from the inner circle
-    let y2 = centerY + (innerCircleRadius + 20) * sin(angleIncrement * i);
-    line(x1, y1, x2, y2);
-  }
+  titleText = createP('The Genesys Experiments:');
+  titleText.position(20, 20); // Upper left corner of the canvas
+  titleText.style('color', 'white');
+  titleText.style('font-size', '30px');
+
+  subTitleText = createP('Map of the Under City');
+  subTitleText.position(20, 60); // Below the title text
+  subTitleText.style('color', 'white');
+  subTitleText.style('font-size', '20px');
 }
 
 function draw() {
@@ -36,7 +39,7 @@ function draw() {
   textSize(20);
 
   strokeWeight(1);
-  stroke(192, 57, 43);
+  stroke(0);
   text("X: " + mouseX, 100, 200);
   text("Y: " + mouseY, 100, 220);
 
@@ -51,14 +54,12 @@ function draw() {
   imageMode(CENTER);
   image(GeneMap, posX, posY, scaledWidth, scaledHeight);
 
-  // Display and check mouse over for each pie slice
   for (let slice of pieSlices) {
     slice.display();
     slice.checkMouseOver();
   }
 }
 
-// PieSlice class definition
 class PieSlice {
   constructor(x, y, diameter, startAngle, endAngle, fillColor, name) {
     this.x = x;
@@ -66,14 +67,13 @@ class PieSlice {
     this.diameter = diameter;
     this.startAngle = startAngle;
     this.endAngle = endAngle;
-    this.fillColor = color(0); // Set initial color to black
-    this.originalColor = fillColor; // Store the original color
+    this.fillColor = color(0);
+    this.originalColor = fillColor;
     this.name = name;
-    this.displayText = false; // Initially, the name text is not displayed
-    this.mouseIsOver = false; // Initially, mouse is not over the slice
+    this.displayText = false;
+    this.mouseIsOver = false;
   }
 
-  // Display method
   display() {
     fill(this.fillColor);
     arc(this.x, this.y, this.diameter, this.diameter, this.startAngle, this.endAngle, PIE);
@@ -84,22 +84,19 @@ class PieSlice {
     }
   }
 
-  // Check if mouse is over the current slice
-checkMouseOver() {
-  let angle = atan2(mouseY - this.y, mouseX - this.x);
-  if (angle < 0) {
-    angle += TWO_PI; // Normalize angle to be between 0 and TWO_PI
-  }
-  if (angle > this.startAngle && angle < this.endAngle && dist(mouseX, mouseY, this.x, this.y) < this.diameter / 2) {
-    // If mouse is over the current slice, change color and display text
-    this.fillColor = this.originalColor;
-    this.displayText = true;
-  } else {
-    // If mouse is not over the current slice, revert color and hide text
-    this.fillColor = color(0, 0, 0, 0);
-    this.displayText = false;
+  checkMouseOver() {
+    let angle = atan2(mouseY - this.y, mouseX - this.x);
+    if (angle < 0) {
+      angle += TWO_PI;
+    }
+    if ((this.name !== "Garbage Sector") && angle > this.startAngle && angle < this.endAngle && dist(mouseX, mouseY, this.x, this.y) < this.diameter / 2) {
+      this.fillColor = this.originalColor;
+      this.displayText = true;
+      garbageSectorText.hide(); // Hide the text when mouse is over any pie slice
+    } else {
+      this.fillColor = color(0, 0, 0, 0);
+      this.displayText = false;
+      garbageSectorText.show(); // Show the text when mouse is not over any pie slice
+    }
   }
 }
-}
-
-
