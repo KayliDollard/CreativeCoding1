1b3ed4a31f259083df90bc5852b32fe07c26d1b2
space-walker11 <kaylidollard@gmail.com>
2024-05-05 23:34:01 -0600

    scream

--- final testing/ahh.js
+++ final testing/ahh.js
@@ -1,7 +1,6 @@
 let GeneMap;
 let pieSlices = [];
-let innerCircleRadius = 550;
-let garbageSectorText;
+let innerCircleRadius = 550; // Radius of the inner circle
 
 function preload() {
   GeneMap = loadImage('mapforGE.png');
@@ -16,10 +15,20 @@ function setup() {
   pieSlices.push(new PieSlice(765, 359, innerCircleRadius, radians(216), radians(288), color('navy'), "Suburbs"));
   pieSlices.push(new PieSlice(765, 359, innerCircleRadius, radians(288), radians(360), color('lightcoral'), "The Pit")); 
 
-  garbageSectorText = createP('Garbage Sector');
-  garbageSectorText.position(725, 39); // Move up by 20 and to the left by 20
-  garbageSectorText.style('color', 'black');
-  garbageSectorText.style('font-size', '30px');
+  // Draw line around the outside of the inner circle
+  let numOfPoints = 360; // Number of points to draw the line
+  let angleIncrement = TWO_PI / numOfPoints;
+  let centerX = 765;
+  let centerY = 359;
+
+  stroke(255); // Set line color to white
+  for (let i = 0; i < numOfPoints; i++) {
+    let x1 = centerX + innerCircleRadius * cos(angleIncrement * i);
+    let y1 = centerY + innerCircleRadius * sin(angleIncrement * i);
+    let x2 = centerX + (innerCircleRadius + 20) * cos(angleIncrement * i); // 20 is the distance from the inner circle
+    let y2 = centerY + (innerCircleRadius + 20) * sin(angleIncrement * i);
+    line(x1, y1, x2, y2);
+  }
 }
 
 function draw() {
@@ -27,7 +36,7 @@ function draw() {
   textSize(20);
 
   strokeWeight(1);
-  stroke(0);
+  stroke(192, 57, 43);
   text("X: " + mouseX, 100, 200);
   text("Y: " + mouseY, 100, 220);
 
@@ -42,12 +51,14 @@ function draw() {
   imageMode(CENTER);
   image(GeneMap, posX, posY, scaledWidth, scaledHeight);
 
+  // Display and check mouse over for each pie slice
   for (let slice of pieSlices) {
     slice.display();
     slice.checkMouseOver();
   }
 }
 
+// PieSlice class definition
 class PieSlice {
   constructor(x, y, diameter, startAngle, endAngle, fillColor, name) {
     this.x = x;
@@ -55,13 +66,14 @@ class PieSlice {
     this.diameter = diameter;
     this.startAngle = startAngle;
     this.endAngle = endAngle;
-    this.fillColor = color(0);
-    this.originalColor = fillColor;
+    this.fillColor = color(0); // Set initial color to black
+    this.originalColor = fillColor; // Store the original color
     this.name = name;
-    this.displayText = false;
-    this.mouseIsOver = false;
+    this.displayText = false; // Initially, the name text is not displayed
+    this.mouseIsOver = false; // Initially, mouse is not over the slice
   }
 
+  // Display method
   display() {
     fill(this.fillColor);
     arc(this.x, this.y, this.diameter, this.diameter, this.startAngle, this.endAngle, PIE);
@@ -72,19 +84,22 @@ class PieSlice {
     }
   }
 
-  checkMouseOver() {
-    let angle = atan2(mouseY - this.y, mouseX - this.x);
-    if (angle < 0) {
-      angle += TWO_PI;
-    }
-    if (angle > this.startAngle && angle < this.endAngle && dist(mouseX, mouseY, this.x, this.y) < this.diameter / 2) {
-      this.fillColor = this.originalColor;
-      this.displayText = true;
-      garbageSectorText.hide(); // Hide the text when mouse is over any pie slice
-    } else {
-      this.fillColor = color(0, 0, 0, 0);
-      this.displayText = false;
-      garbageSectorText.show(); // Show the text when mouse is not over any pie slice
-    }
+  // Check if mouse is over the current slice
+checkMouseOver() {
+  let angle = atan2(mouseY - this.y, mouseX - this.x);
+  if (angle < 0) {
+    angle += TWO_PI; // Normalize angle to be between 0 and TWO_PI
+  }
+  if (angle > this.startAngle && angle < this.endAngle && dist(mouseX, mouseY, this.x, this.y) < this.diameter / 2) {
+    // If mouse is over the current slice, change color and display text
+    this.fillColor = this.originalColor;
+    this.displayText = true;
+  } else {
+    // If mouse is not over the current slice, revert color and hide text
+    this.fillColor = color(0, 0, 0, 0);
+    this.displayText = false;
   }
 }
+}
+
+
